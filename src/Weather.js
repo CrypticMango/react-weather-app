fde92afa0b0f603b80adb19da67afc988fcf2480
CrypticMango <79679706+CrypticMango@users.noreply.github.com>
2021-09-14 20:59:52 -0600

    fixed errors and warnings

--- src/Weather.js
+++ src/Weather.js
@@ -2,39 +2,39 @@ import React from "react";
 
 export default function Weather() {
    return ( <div>
-        <div class="card-whole-app" style={{width: '65rem'}}>
-          <div class="card-body">
-            <h5 class="card-title"></h5>
-            <p class="card-text"></p>
+        <div className="card-whole-app" style={{width: '65rem'}}>
+          <div className="card-body">
+            <h5 className="card-title"></h5>
+            <p className="card-text"></p>
           </div>
 
-          // search your city name
+          {/* search your city name */}
           
           <form id="search-form">
-            <div class="input-group" style={{width: '45rem'}}>
+            <div className="input-group" style={{width: '45rem'}}>
               <input
                 id="city-input"
                 type="text"
-                class="form-control"
+                className="form-control"
                 placeholder="Enter your city name"
                 aria-label="users city with two button addons"
               />
-              <button class="btn btn-primary" type="submit">
+              <button className="btn btn-primary" type="submit">
                 Search
               </button>
     
-              <button class="btn btn-primary" id="current-button" type="button">
+              <button className="btn btn-primary" id="current-button" type="button">
                 Current
               </button>
     
             </div>
           </form>
     
-          // Center Info
+          {/* Center Info */}
           
-          <div class="container-main">
-            <div class="row">
-              <div class="col-5 city-info">
+          <div className="container-main">
+            <div className="row">
+              <div className="col-5 city-info">
                 <br />
     
                 <h1 id="current-city">
@@ -62,48 +62,48 @@ export default function Weather() {
     
                 <div id="unit-conversion-buttons">
                     
-                  <a id="celsius-button" href="">C</a> | <a id="fahrenheit-button" href="">F</a> 
+                  <a id="celsius-button" href="/">C</a> | <a id="fahrenheit-button" href="/">F</a> 
                   
                   </div>
     
               </div>
-              <div class="col-3 align-self-center">
-                <p class="large-temp">
+              <div className="col-3 align-self-center">
+                <p className="large-temp">
                   <img id="icon" src="http://openweathermap.org/img/wn/10d@2x.png" alt=""/>  
                   <span id="current-temperature">25°C</span>
                  </p>
               </div>
               
-                  <div class="col-4 today-column">
-                    <p class="today-header">
+                  <div className="col-4 today-column">
+                    <p className="today-header">
                       Today
                     </p>
                     <p>
                       Morning
-                      HOT <i class="fas fa-temperature-high"></i>
+                      HOT <i className="fas fa-temperature-high"></i>
                     </p>
     
                     <p>
                       Noon
-                      SUNNY <i class="fas fa-thermometer-half"></i>
+                      SUNNY <i className="fas fa-thermometer-half"></i>
                     </p>
     
                     <p>
                       Evening
-                      MISTING <i class="fas fa-cloud-rain"></i>
+                      MISTING <i className="fas fa-cloud-rain"></i>
                     </p>
     
                     <p>
                       Night
-                      CHILLY <i class="fas fa-temperature-low"></i>
+                      CHILLY <i className="fas fa-temperature-low"></i>
                     </p>
                   </div>
     
-                  // days of the week
+                  {/* days of the week */}
     
-                  <div class="col-8 align-self-center">
-                    <div class="weather-by-the-day" id="weather-forecast">
-                      <i class="fas fa-sun daily-icon"></i>
+                  <div className="col-8 align-self-center">
+                    <div className="weather-by-the-day" id="weather-forecast">
+                      <i className="fas fa-sun daily-icon"></i>
                       <br />
                       MON
                       <br />
@@ -111,9 +111,9 @@ export default function Weather() {
                     </div>
                   </div>
 
-        <p class="my-name">
-          <a href="https://github.com/CrypticMango/weather-app-project"  target="_blank" rel="noreferrer" class="name-link">open-source code</a> by Lisa Allen. Hosted on
-          <a href="https://www.netlify.com"  target="_blank" rel="noreferrer" class="name-link">Netlify.</a>
+        <p className="my-name">
+          <a href="https://github.com/CrypticMango/weather-app-project"  target="_blank" rel="noreferrer" className="name-link">open-source code</a> by Lisa Allen. Hosted on
+          <a href="https://www.netlify.com"  target="_blank" rel="noreferrer" className="name-link">Netlify.</a>
         </p>
         <script src="src/app.js"></script>
         </div>
