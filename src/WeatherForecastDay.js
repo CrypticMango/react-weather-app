69446bee944c15ea561c5949b546dead8a439d0b
CrypticMango <79679706+CrypticMango@users.noreply.github.com>
2021-09-24 20:06:56 -0600

    bug fixes and multiple weather forecast days added

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -9,13 +9,13 @@ export default function WeatherForecastDay(props) {
     }
 
     function minTemperature() {
-        let temperature = Math.round(props.data.temp.max)
+        let temperature = Math.round(props.data.temp.max);
         return `${temperature}`;
     }
 
     function day() {
         let date = new Date(props.data.dt * 1000);
-        let day = date.getDay()
+        let day = date.getDay();
 
         let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
@@ -25,7 +25,7 @@ export default function WeatherForecastDay(props) {
     return (
         <div>
             <div className="WeatherForecast-day">{day()}</div>
-                <WeatherIcon code={props.weather[0].icon} size={36}/>
+                <WeatherIcon code={props.data.weather[0].icon} size={36}/>
                     <div className="WeatherForecast-temperatures">
                         <span className="WeatherForecast-temperature-max">{maxTemperature()}
                         </span>
